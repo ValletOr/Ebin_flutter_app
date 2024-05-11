90c8e3097af9b6312aec7cd823ebde323364a759
ValletOr <mtvac11@gmail.com>
2024-05-11 14:13:15 +0800

    create common appbar to separate file. refactor existing widgets

--- lib/appCard.dart
+++ lib/appCard.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:enplus_market/models/CardModel.dart';
 import 'package:enplus_market/ImageDetailScreen.dart';
+import 'commonAppBar.dart';
+
 class appCard extends StatelessWidget {
   final CardModel card;
   final TextEditingController _searchController = TextEditingController();
@@ -13,62 +15,7 @@ class appCard extends StatelessWidget {
     return DefaultTabController(
       length: 3,
       child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          surfaceTintColor: Colors.white,
-          bottom: const TabBar(
-            // labelColor: Color(0xFFFD9330),
-            // unselectedLabelColor: Color(0xFF212529),
-            // indicatorColor: Color(0xFFFD9330) ,
-            tabs: [
-              Tab(text: "Приложения"),
-              Tab(text: "Тестирование"),
-              Tab(text: "Установленные"),
-            ],
-            labelPadding: EdgeInsets.only(right: 1, top: 1),
-          ),
-          title: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 1.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _searchController,
-                    decoration: InputDecoration(
-                      hintText: 'Поиск приложения',
-                      contentPadding: const EdgeInsets.all(5.0),
-                      prefixIcon: IconButton(
-                        icon: const Icon(Icons.clear),
-                        onPressed: () => _searchController.clear(),
-                      ),
-                      suffixIcon: IconButton(
-                        icon: const Icon(Icons.search),
-                        onPressed: () {
-                          // Perform the search here
-                        },
-                      ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(48.0),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(width: 5),
-                Expanded(
-                  flex: 0,
-                  child: IconButton(
-                      onPressed: () => {},
-                      icon: const Icon(
-                        Icons.account_circle,
-                        size: 48,
-                      ),
-                      style: IconButton.styleFrom(
-                          padding: const EdgeInsets.only(left: 12.0))),
-                ),
-              ],
-            ),
-          ),
-        ),
+        appBar: CommonAppBar(),
         body: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
